d8a3dd710007ebff9684b286e4d8a4f5493cd378
antonpetkov88 <63842889+antonpetkov88@users.noreply.github.com>
2020-04-17 17:20:47 +0300

    Add files via upload

---
+++ HWAPR17.js
@@ -0,0 +1,18 @@
+//Ex.1//
+const area = function(a, b){
+   return a*b
+}
+console.log(area (11,12));
+
+//Ex.2//
+let c = 5
+let d = 5
+const crazySum = function (c, d){
+    return c + d
+   }
+if(c === d){
+    console.log(crazySum*3);
+} else (c != d); {
+    console.log(crazySum);
+}
+ 
