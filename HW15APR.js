e7e9206b8fd75d96f3e625a4dda9bbee0cf5edbe
antonpetkov88 <63842889+antonpetkov88@users.noreply.github.com>
2020-04-20 15:41:57 +0300

    Add files via upload
    HOMEWORK 15 APR

---
+++ HW15APR.js
@@ -0,0 +1,29 @@
+console.log(12 + 20);
+let x = 12;
+console.log(x)
+let name = "John Doe";
+console.log(name);
+console.log(12 - x);
+let name1 = "john";
+let name2 = "John";
+if (name1 == name2) {
+    console.log("Names are equal");
+} else {
+    console.log("Names are not equal");
+}
+if (name1.toLowerCase() == name2.toLowerCase()) {
+    console.log("Names are the same")
+}
+let y = 8;
+if (y == 1) {
+    console.log("one");
+} else if (y == 2) {
+    console.log("two");
+} else if (y == 8) {
+    console.log("eight");
+}
+let students = 30;
+let tutors = 5;
+let check;
+check = (students > tutors) ? "True" : "False";
+console.log(check);
